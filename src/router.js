79caee861f6883a0d2601dbd0f41dd041af6302b
Lee-Seungje <moondgod@gmail.com>
2023-07-02 14:39:44 +0900

    Route complete

--- src/router.js
+++ src/router.js
@@ -1,16 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import { Home, About } from "./views";
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
+export const router = new VueRouter({
   mode: "history",
   routes: [
     { path: "/", component: Home },
     { path: "/about", component: About },
   ],
 });
-
-export default router;
