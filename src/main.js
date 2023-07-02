e08a25520191c3dd56361e3c5664f4e2e2d509f8
Lee-Seungje <moondgod@gmail.com>
2023-07-02 14:16:09 +0900

    Add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import { router } from "./router";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to About!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewAbout",
+};
+</script>
